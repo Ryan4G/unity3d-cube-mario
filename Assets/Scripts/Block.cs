90006cf0d41ff14e87730f6776a92fe08a70054e
ryan4gitee <rliang.lai@outlook.com>
2022-01-09 23:21:57 +0800

    Add prefabs

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public Transform block;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (var x = 0; x < 5; x++)
+        {
+            Instantiate(block, new Vector3(transform.position.x + x, transform.position.y, transform.position.z), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public float jumpSpeed = 15.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateControl();
+    }
+
+    void UpdateControl()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+
+        var jump = Input.GetButtonDown("Jump");
+
+        Vector3 move = Vector3.zero;
+
+        move.x = horizontal * speed;
+
+        if (jump)
+        {
+            move.y = jumpSpeed;
+        }
+
+        transform.Translate(move * Time.deltaTime);
+
+    }
+}
