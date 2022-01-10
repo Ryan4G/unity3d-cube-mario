5aea9430126ce155eadba5189ceacbc7d4a85c36
ryan4gitee <rliang.lai@outlook.com>
2022-01-10 23:41:37 +0800

    Add Scripts.

---
+++ Assets/Scripts/Touch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touch : MonoBehaviour
+{
+    public GameObject prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                Instantiate(prefab, hitInfo.point, Quaternion.identity);
+            }
+        }
+    }
+}
