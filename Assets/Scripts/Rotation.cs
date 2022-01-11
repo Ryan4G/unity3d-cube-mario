33291c36720185941aa66969507f4a99a448652b
ryan4gitee <rliang.lai@outlook.com>
2022-01-11 23:46:05 +0800

    Add scripts.

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public float rotationZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotationZ = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotationZ += Time.deltaTime * 10;
+
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, rotationZ));
+    }
+}
